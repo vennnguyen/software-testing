4a02f42a219dce4499d3aa64d43df1367495090e
Thuat3Tuoi <itdontsleep@gmail.com>
2025-11-30 18:30:27 +0700

    update

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,16 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 
   reporter: "cypress-multi-reporters",
 
   reporterOptions: {
-    reporterEnabled: "spec, junit",
-    junitReporterOptions: {
+    reporterEnabled: "spec, mocha-junit-reporter",
+    mochaJunitReporterReporterOptions: {
       mochaFile: "reports/e2e-report-[hash].xml",
     },
   },
